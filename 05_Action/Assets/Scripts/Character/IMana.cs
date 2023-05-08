9e2fb7ee1900e259403f2376fa2edda56aaea78c
go2665 <77224762+go2665@users.noreply.github.com>
2023-05-08 12:06:52 +0900

    Action - Mana 인터페이스 작성

--- 05_Action/Assets/Scripts/Character/IMana.cs
+++ 05_Action/Assets/Scripts/Character/IMana.cs
@@ -3,36 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-interface IHealth
+interface IMana
 {
     /// <summary>
-    /// 살았는지 죽었는지 확인하는 프로퍼티
+    /// MP 확인 및 설정용 프로퍼티
     /// </summary>
-    bool IsAlive { get; }
+    float MP { get; set; }
 
     /// <summary>
-    /// HP 확인 및 설정용 프로퍼티
+    /// 최대 MP를 확인하는 프로퍼티
     /// </summary>
-    float HP { get; set; }
+    float MaxMP { get; }    
 
     /// <summary>
-    /// 최대 HP를 확인하는 프로퍼티
+    /// MP 변경을 알리기 위한 델리게이트를 설정하고 사용하는 프로퍼티
     /// </summary>
-    float MaxHP { get; }    
+    Action<float> onManaChange { get; set; }
 
     /// <summary>
-    /// HP 변경을 알리기 위한 델리게이트를 설정하고 사용하는 프로퍼티
+    /// 마나를 지속적으로 증가시켜 주는 함수. 초당 totalRegen/duration만큼씩 회복
     /// </summary>
-    Action<float> onHealthChange { get; set; }
-
-    /// <summary>
-    /// 사망처리용 함수
-    /// </summary>
-    void Die();
-
-    /// <summary>
-    /// 사망을 알리기 위한 델리게이트를 설정하고 사용하는 프로퍼티
-    /// </summary>
-    Action onDie { get; set; }
+    /// <param name="totalRegen">전체 회복량</param>
+    /// <param name="duration">전체 회복하는데 걸리는 시간</param>
+    void ManaRegenerate(float totalRegen, float duration);
 
 }
