f32242f8771234e959e75715407b044654bbe8e0
go2665 <77224762+go2665@users.noreply.github.com>
2023-05-12 12:05:34 +0900

    Action - 플레이어 가상 카메라 추가 + 미니맵 카메라 스크립트 작성중

--- 05_Action/Assets/Scripts/Test/Test_Minimap.cs
+++ 05_Action/Assets/Scripts/Test/Test_Minimap.cs
@@ -7,6 +7,16 @@ public class Test_Minimap : Test_Base
     void Start()
     {
         ItemFactory.MakeItem(ItemCode.Ruby, 10);
-        
+
+        GameObject obj = ItemFactory.MakeItem(ItemCode.Emerald);
+        obj.transform.position = new Vector3(5, 0, 0);
+        obj = ItemFactory.MakeItem(ItemCode.Emerald);
+        obj.transform.position = new Vector3(-5, 0, 0);
+        obj = ItemFactory.MakeItem(ItemCode.Emerald);
+        obj.transform.position = new Vector3(0, 0, 5);
+        obj = ItemFactory.MakeItem(ItemCode.Emerald);
+        obj.transform.position = new Vector3(0, 0, -5);
+
+
     }
 }
