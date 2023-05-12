d1fdbcec9ac67971e2fe2b626e5973f051c5cb17
go2665 <77224762+go2665@users.noreply.github.com>
2023-05-12 14:25:03 +0900

    Action - 슬라임 생성
    애니메이션 설정, 웨이포인트

--- 05_Action/Assets/Scripts/Character/Player/PlayerController.cs
+++ 05_Action/Assets/Scripts/Character/Player/PlayerController.cs
@@ -121,8 +121,11 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         InventoryUI invenUI = GameManager.Inst.InvenUI;
-        invenUI.onInventoryOpen += inputActions.Player.Attack.Disable;  // 인벤토리가 열릴때는 공격 안함
-        invenUI.onInventoryClose += inputActions.Player.Attack.Enable;  // 인벤토리가 닫히면 다시 공격함
+        if (invenUI)
+        {
+            invenUI.onInventoryOpen += inputActions.Player.Attack.Disable;  // 인벤토리가 열릴때는 공격 안함
+            invenUI.onInventoryClose += inputActions.Player.Attack.Enable;  // 인벤토리가 닫히면 다시 공격함
+        }
     }
 
     private void OnEnable()
